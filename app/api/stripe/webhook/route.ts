fc95a14187cbfe142d6d7396f15bdf16b1563568
mac14 <mac14@mac14s-MacBook-Pro.local>
2025-11-20 11:11:10 +0300

    remove stripe api's

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,5 +1,5 @@
-import Stripe from 'stripe';
-import { handleSubscriptionChange, stripe } from '@/lib/payments/stripe';
+// import Stripe from 'stripe';
+// import { handleSubscriptionChange, stripe } from '@/lib/payments/stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
@@ -8,10 +8,10 @@ export async function POST(request: NextRequest) {
   const payload = await request.text();
   const signature = request.headers.get('stripe-signature') as string;
 
-  let event: Stripe.Event;
+  //let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(payload, signature, webhookSecret);
+    //event = stripe.webhooks.constructEvent(payload, signature, webhookSecret);
   } catch (err) {
     console.error('Webhook signature verification failed.', err);
     return NextResponse.json(
@@ -20,15 +20,15 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  switch (event.type) {
-    case 'customer.subscription.updated':
-    case 'customer.subscription.deleted':
-      const subscription = event.data.object as Stripe.Subscription;
-      await handleSubscriptionChange(subscription);
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
+  // switch (event.type) {
+  //   case 'customer.subscription.updated':
+  //   case 'customer.subscription.deleted':
+  //     const subscription = event.data.object as Stripe.Subscription;
+  //     await handleSubscriptionChange(subscription);
+  //     break;
+  //   default:
+  //     console.log(`Unhandled event type ${event.type}`);
+  // }
 
   return NextResponse.json({ received: true });
 }
